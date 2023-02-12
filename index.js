3aeaf466258652fdb8ebf82d2fb0e6d0cd9a62e1
jonathan gober <Gober.jonathan@gmail.com>
2023-02-11 20:56:41 -0500

    Trying something new

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-import Twit from "twit"
-import dotenv from "dotenv"
-import { data } from "./tweets.js"
-let selectAtRandom = data[Math.floor(Math.random() * data.length)]
+import Twit from "twit";
+import dotenv from "dotenv";
+import { chatGPT } from "./lib/chatGPT.js";
+
 dotenv.config();
 
 const T = new Twit({
-    consumer_key: process.env.API_KEY,
-    consumer_secret: process.env.API_SECRET,
-    access_token: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  consumer_key: process.env.API_KEY,
+  consumer_secret: process.env.API_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 });
-const tweet = () => {
-    const text = selectAtRandom;
+const tweet = async () => {
+    const text = `Letting ChatGPT ask us questions: ${await chatGPT(`Ask me a question instead. Dont give me any answers`)} #ChatGPT #AI`
 
-    const onFinish = (err, reply) => {
-        if (err) {
-            console.log("Error: ", err.message);
-        } else {
-            console.log("Success: ", reply);
-        }
-    };
+  const onFinish = (err, reply) => {
+    if (err) {
+      console.log("Error: ", err.message);
+    } else {
+      console.log("Success: ", reply);
+    }
+  };
 
-    T.post("statuses/update", { status: text }, onFinish);
+  T.post("statuses/update", { status: text }, onFinish);
 };
 
 tweet();
--- test.js
+++
@@ -1,5 +0,0 @@
-import { data } from "./tweets.js";
-let dataLength = data.length;
-console.log(dataLength);
-let selectAtRandom = data[Math.floor(Math.random() * data.length)];
-console.log(selectAtRandom);
