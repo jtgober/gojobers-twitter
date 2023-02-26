20ec138d0bbe538be7b1955d05a321c2fd1030f0
jonathan gober <Gober.jonathan@gmail.com>
2023-02-26 08:42:08 -0500

    updated to post picutures

---
+++ specs/chatGPT.spec.js
@@ -0,0 +1,18 @@
+import { chatGPT } from "../lib/chatGPT.js";
+let randomNumber = () => {
+  // min and max included
+  return Math.floor(Math.random() * 2) + 1;
+};
+let myNum = randomNumber();
+console.log("🚀 ~ file: chatGPT.spec.js:8 ~ myNum:", myNum);
+
+switch (myNum) {
+  case 1:
+    console.log(await chatGPT("generate a cute prompt for a realistic dog"));
+    break;
+  case 2:
+    console.log(await chatGPT("generate a cute prompt for a realistic cat"));
+    break;
+  default:
+    console.log(await chatGPT("generate a cute prompt for a realistic axolotl"));
+}
