12d42a9a21012d6dee63c9010a8331b50015255d
jonathan gober <Gober.jonathan@gmail.com>
2023-02-25 22:35:37 -0500

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import { chatGPT } from "./lib/chatGPT.js";
 import { generateDalleImage } from "./lib/dalle-e.js";
 import { getRandomPokemon } from "./lib/poke-api.js";
-const util = require("util");
+import util from "util";
 
 dotenv.config();
 
